55ba5aaca79e50a7c665daf71a5659c929a57295
liyicky <liyicky@gmail.com>
2016-02-16 05:44:04 -0500

    Updated convertToMP4.sh

--- convertToMP4.sh
+++ convertToMP4.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
+# Install ffmpeg: brew install ffmpeg
 
 PATH_ARG="$1"
 
 function convert_all_to_mp4() {
 
-  echo $PATH_ARG
-
-  if [[ -z $PATH_ARG ]] || [![ -d $PATH_ARG ]] ; then
-	echo "Please provide a path. (Include '/')"
+  if [[ -z $PATH_ARG ]] || [[ ! -d $PATH_ARG ]] ; then
+	echo "~~ Please provide a path (Include '/') ~~"
 	exit 1
   fi
 
@@ -23,11 +22,13 @@ function convert_all_to_mp4() {
 function convert() {
 	FILETYPE=$1
 
+	echo "~~ CONVERTING $FILETYPE ~~"
+
 	for file in $PATH_ARG*$FILETYPE ; do
 	    local bname=$(/usr/bin/basename "$file" $FILETYPE)
 	    local mp4name="$bname.mp4"
-	    /usr/local/bin/ffmpeg -i "$file" "$PATH_ARG$mp4name"
-    done
+	    /usr/local/bin/ffmpeg -i "$file" "$PATH_ARG$mp4name" -hide_banner -loglevel verbose
+	done
 }
 
 convert_all_to_mp4
