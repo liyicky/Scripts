98a6dbb4931409eabb087598d329c511e92a2c08
liyicky <liyicky@gmail.com>
2019-02-16 23:12:56 +0900

    more audio stuff

--- convertToMP4.sh
+++ convertToMP4.sh
@@ -17,18 +17,41 @@ function convert_all_to_mp4() {
   convert .wmv
   convert .3gp
   convert .mov
+  convert .mkv
+  convert .srt
+  convert .flac
 }
 
 function convert() {
-	FILETYPE=$1
-
 	echo "~~ CONVERTING $FILETYPE ~~"
+	local FILETYPE=$1
+  local VIDEO_COUNT=$(count $FILETYPE)
+  local new_dir=$PATH_ARG/Converted_$FILETYPE
+
 
 	for file in $PATH_ARG*$FILETYPE ; do
 	    local bname=$(/usr/bin/basename "$file" $FILETYPE)
 	    local mp4name="$bname.mp4"
 	    /usr/local/bin/ffmpeg -i "$file" "$PATH_ARG$mp4name" -hide_banner -loglevel verbose
-	done
+  done
+
+  if [ $VIDEO_COUNT > 0 ] ; then 
+    mkdir $new_dir
+    mv $mp4name $new_dir
+  fi 
+
+}
+
+function count() {
+  local FILETYPE=$1
+  local COUNTER=0
+
+  for video in $FILETYPE
+  do
+    let COUNTER=$COUNTER+1
+  done
+
+  echo $COUNTER
 }
 
 convert_all_to_mp4
