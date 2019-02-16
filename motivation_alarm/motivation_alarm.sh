d0d89a39154cdcdd06d2561d19372310ed580df8
liyicky <liyicky@gmail.com>
2019-02-16 23:50:30 +0900

    alarm clock script

---
+++ motivation_alarm/motivation_alarm.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+osascript -e "set volume 10"
+
+ls ./books/**.m** | sort -R | tail -1 | while read file; do
+    terminal-notifier -title "Motivation Alarm" -message "Playing $file";
+    open $file;
+done
