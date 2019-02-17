57a96ce8ecdfc9020f5cb518ce55f9edeb9918f5
liyicky <liyicky@gmail.com>
2019-02-17 15:31:27 +0900

    fixed a thing

--- motivation_alarm/motivation_alarm.sh
+++ motivation_alarm/motivation_alarm.sh
@@ -2,7 +2,7 @@
 
 osascript -e "set volume 10"
 
-ls ./books/**.m** | sort -R | tail -1 | while read file; do
+ls books/**.m** | sort -R | tail -1 | while read file; do
     terminal-notifier -title "Motivation Alarm" -message "Playing $file";
     open $file;
 done
