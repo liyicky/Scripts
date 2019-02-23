4ef26f8be6b010dc72a36b30771a523b01dbbb44
liyicky <liyicky@gmail.com>
2019-02-23 12:02:07 +0900

    enable terminal control

--- motivation_alarm/motivation_alarm.sh
+++ motivation_alarm/motivation_alarm.sh
@@ -4,5 +4,5 @@ osascript -e "set volume 10"
 
 ls ~/Catode/Scripts/motivation_alarm/books/**.m** | sort -R | tail -1 | while read file; do
     terminal-notifier -title "Motivation Alarm" -message "Playing $file";
-    mpg123 -v $file;
+    osascript -e 'tell application "Terminal" to do script "mpg123 --title -v '$file'"';
 done
